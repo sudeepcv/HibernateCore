3dcde3cb1444a5f993f0eaf211bf2a2aea99b432
Sudeep-cv <basicsinjava@gmail.com>
2019-06-06 09:50:38 +0000

    h2 from maven config

---
+++ src/main/java/com/sudeep/App.java
@@ -0,0 +1,16 @@
+package com.sudeep;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+    }
+}
